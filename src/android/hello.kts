5ca25d11b6168e5401f6bb5fc8675caf1c45ee47
Ethan <ethanedwinhughes@gmail.com>
2025-02-19 11:42:25 -0600

    More testing programs to show how Kotlin handles functions

--- src/android/hello.kts
+++ src/android/hello.kts
@@ -1,4 +1,14 @@
+fun test1(first: String): Unit {
+    println("Hello" + " " + first)
+}
+
+fun test2(add: Int): Int {
+    return add + 3
+}
+
 fun main(){
     println("Hello World")
+    test1("Ethan")
+    println(test2(7))
 }
 main()
