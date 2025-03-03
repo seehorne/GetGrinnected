a638ac39d2d31920a149088e16db55762d83f03e
Ethan <ethanedwinhughes@gmail.com>
2025-03-02 18:21:13 -0600

    More testing programs to show how Kotlin handles functions

--- src/android/hello.kts
+++ src/android/hello.kts
@@ -6,9 +6,31 @@ fun test2(add: Int): Int {
     return add + 3
 }
 
+fun recursive(iterate: Int): Int{
+    if (iterate == 100) {
+        return iterate
+    }
+    return recursive(iterate + 1)
+}
+
+fun loop(){
+val colors = listOf("Red", "Green", "Blue")
+for (color in colors) {
+    print("$color \n")
+    }
+}
+
+fun add(a: Int, b: Int): Int {
+    val sum = a + b
+    return sum
+}
 fun main(){
     println("Hello World")
     test1("Ethan")
     println(test2(7))
+    println(recursive(1))
+    val result = add(5, 3)
+    println(result)
+    loop()
 }
 main()
