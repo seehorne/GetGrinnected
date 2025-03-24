07b85ca6f1fa58800cc27c69fbe1990526a33532
AnthonyS25223 <anthonyschwindt@gmail.com>
2025-03-23 20:59:00 -0500

    Update Database
    Updated the sql code here to mirror that for which I used on the reclaim server. Needed to make some adjustments as certain types didn't work in MySQL.

--- src/backend/Database/GetGrinnectedDB.sql
+++ src/backend/Database/GetGrinnectedDB.sql
@@ -1,32 +1,32 @@
 CREATE DATABASE IF NOT EXISTS getgrinnecteddb;
 
-CREATE TABLE IF NOT EXISTS events(
-    eventid INTEGER AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS events (
+    eventid INT AUTO_INCREMENT PRIMARY KEY,
     event_name TEXT,
     event_description TEXT,
     event_location TEXT,
-    organizations VARCHAR(MAX),
-    rsvp INTEGER,
+    organizations JSON,
+    rsvp INT,
     event_date DATE,
     event_time TIME,
-    tags VARCHAR(MAX),
-    event_private INTEGER,
-    repeats INTEGER,
-    event_image IMAGE,
-    is_draft INTEGER
+    tags JSON,
+    event_private INT,
+    repeats INT,
+    event_image LONGBLOB,
+    is_draft INT
 );
 
 CREATE TABLE IF NOT EXISTS accounts(
-    accountid INTEGER AUTO_INCREMENT PRIMARY KEY,
+    accountid INT AUTO_INCREMENT PRIMARY KEY,
     account_name TEXT,
     email TEXT,
     password TEXT,
     salt TEXT,
-    profile_picture IMAGE,
-    favorited_events VARCHAR(MAX),
-    favorited_orgs VARCHAR(MAX),
-    drafted_events VARCHAR(MAX),
-    favorited_tags VARCHAR(MAX),
+    profile_picture LONGBLOB,
+    favorited_events JSON,
+    favorited_orgs JSON,
+    drafted_events JSON,
+    favorited_tags JSON,
     account_description TEXT,
-    account_role INTEGER
+    account_role INT
 );
