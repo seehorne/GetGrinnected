92f0f91fde8c673790bdc9f25bf74880623b86a9
Ellie Seehorn <seehorne@gmail.com>
2025-03-26 15:05:32 -0500

    Maintain formatting when scrape new events + no duplicates

--- src/backend/scrape.js
+++ src/backend/scrape.js
@@ -1,7 +1,26 @@
 const fs = require("fs");
 const url = 'https://events.grinnell.edu/live/json/events/response_fields/all/paginate'
 
+function processExisting(){
+  try{
+    events = fs.readFileSync('event_data.json', 'utf-8');
+    current_events = JSON.parse(events)
+      existingIDs = new Set();
+      if (current_events.data && Array.isArray(current_events.data)){
+        current_events.data.forEach(event =>{
+          existingIDs.add(event.ID);
+      });
+      return existingIDs;
+      }
+  }
+  catch(error){
+    console.error('Error fetching the JSON data:', error);
+  }
+}
+
 async function scrapeData(url) {
+  existingIDs = await processExisting();
+  const appendPromises = [];
   fetch(url)
   .then(response => {
     if (!response.ok) {
@@ -9,12 +28,17 @@ async function scrapeData(url) {
     }
     return response.json();
   })
-  .then(events => {
+  .then(async events => {
     //console.log(events.data);
-    event_info = {};
+    existing_events = fs.readFileSync('event_data.json', 'utf-8');
+    lines = existing_events.split('\n');
+    updatedLines = lines.slice(0, -2); //remove last two lines
+    fs.writeFileSync('event_data.json', updatedLines.join('\n'));
     counter = 0;
     if (events.data && Array.isArray(events.data)) {
       events.data.forEach(event => {
+        if (!existingIDs.has(event.id)){
+        existingIDs.add(event.id);
         event_info = {};
         event_info["Title"] = event.title;
         event_info["Date"] = event.date;
@@ -26,16 +50,24 @@ async function scrapeData(url) {
         event_info["Tags"]= event.tags;
         event_info["ID"]= event.id;
         stringify_event = JSON.stringify(event_info);
-        if (counter != 0){
+        if (counter != 0 || lines.length >= 2){ //not first event to be added ever
             stringify_event = ',\n'+stringify_event;
         }
         counter++;
-        fs.appendFile('event_data.json', stringify_event, function(err){
-            if(err) throw err;
-            console.log('WRITING TO JSON')
-            });
-      });
+        appendPromises.push(fs.appendFile('event_data.json', stringify_event, function(err){
+          if(err) throw err;
+          console.log('WRITING TO JSON')
+          }));
+        }
+      }
+    );
     }
+    await Promise.all(appendPromises);
+    const close_file = '\n]\n}'
+    fs.appendFile('event_data.json', close_file, function(err){
+      if(err) throw err;
+      console.log('WRITING TO JSON')
+      });
   })
   .catch(error => {
     console.error('Error fetching the JSON data:', error);
