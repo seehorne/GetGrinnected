43e08a6e0f3d95fd6fe444f5d8f715a8991e0fa3
almond Heil <almond@almendra.dev>
2025-03-30 17:02:40 -0500

    Write more in api.js (currently makes npm test hang)

--- src/backend/test/test_api.js
+++ src/backend/test/test_api.js
@@ -1,14 +1,13 @@
-const { it, describe } = require('node:test');
+const { test, describe } = require('node:test');
 const assert = require('node:assert/strict');
+const api = require('../api.js');
 
-describe('name of a testing suite', () => {
+describe('API unit tests', () => {
 
-  it('should add numbers', () => {
-    assert.strictEqual(4, 2+2);
-  });
-
-  it('should subtract numbers', () => {
-    assert.strictEqual(0, 1-1);
+  test('element that exists', () => {
+    const expected = {"Title":"“Cannon, People, Pedagogies”","Date":"April 2","Time":"4:30 p.m. - 8 p.m.","StartTimeISO":"2025-04-02T16:30:00-05:00","EndTimeISO":"2025-04-02T20:00:00-05:00","AllDay?":false,"Location":"HSSC A1231 - Multi-purpose Room","Description":"\n  Interactive workshop with Music Department guest Danielle Brown.\n","Audience":["Faculty &amp; Staff","Students"],"Org":"Music Department","Tags":["Music"],"ID":30268}
+    const actual = api.get_event_by_id(30268);
+    assert.strictEqual(expected, actual);
   });
 
 });
