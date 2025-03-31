b880429856052f421d4a3bf06a6a3c8347b52a3d
Ellie Seehorn <seehorne@gmail.com>
2025-03-31 08:32:45 -0500

    Ensure all targets have a signed team

--- src/ios/GetGrinnected/MyBuildToolPlugin/Package.swift
+++ src/ios/GetGrinnected/MyBuildToolPlugin/Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "MyBuildToolPlugin",
-    products: [
-        // Products can be used to vend plugins, making them visible to other packages.
-        .plugin(
-            name: "MyBuildToolPlugin",
-            targets: ["MyBuildToolPlugin"]),
-    ],
+//    products: [
+//        // Products can be used to vend plugins, making them visible to other packages.
+//        .plugin(
+//            name: "MyBuildToolPlugin",
+//            targets: ["MyBuildToolPlugin"]),
+//    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
