f5d7acab796f708629673252f521d0ca7d3d01a5
EthanEdwinHughes <129095859+EthanEdwinHughes@users.noreply.github.com>
2025-04-02 01:36:21 -0500

    Added Color background and scrollable event tags to the home page. This is very shoddy work but I want to save it.

--- src/android/GetGrinnected/MyApplication/app/src/main/java/com/example/myapplication/HomeScreen.kt
+++ src/android/GetGrinnected/MyApplication/app/src/main/java/com/example/myapplication/HomeScreen.kt
@@ -1,13 +1,8 @@
 package com.example.myapplication
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -17,16 +12,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.test.services.storage.file.PropertyFile
 import androidx.compose.foundation.layout.Column as Column1
 
 /**
