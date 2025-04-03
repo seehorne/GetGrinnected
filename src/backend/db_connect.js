639cdb153bb17e1c3912f2523852b1198a1eca12
AnthonyS25223 <anthonyschwindt@gmail.com>
2025-04-03 08:46:58 -0500

    Implement simple get and set functions and dropping expired events for database
    Done:
    - getEvents function
    - createAccount function
    - getAccount function
    - dropExpiredEvents function
    - verifyLogin function
    
    TODO:
    - Server Side testing
    - automated testing (this will require a test fixture to work)
    - Documentation of functions (Most should be self explanatory by name but params may not be)

--- src/backend/db_connect.js
+++ src/backend/db_connect.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 const fs = require ('fs');
+const bcrypt = require('bcrypt');
 
 /* Important note a .env file needs to exist on any machine hoping to run this with the necessary 
 *  information in its contents (this isn't hard but will need to be added on the server side)
@@ -66,9 +67,95 @@ function combineTags(event){
     const tags = Array.isArray(event.tags) ? event.tags : []; 
     const Audience = Array.isArray(event.Audience) ? event.Audience : [];
     const combined_tags = tags.concat(Audience);
-    return combined_tags
+    return combined_tags;
 }
 
+async function getEvents(){
+    const [events] = await pool.query("SELECT * FROM events");
+    return events;
+}
 
+async function getAccount(username){
+    const [account] = await pool.query(`
+       SELECT * 
+       FROM accounts
+       WHERE account_name = ?
+        `, [username]);
+        return account[0]; 
+}
+
+async function createAccount(username, email, password){
+
+    const existing_account = await getAccount(username);
+
+    if(existing_account){
+        throw new Error("User already exists.");
+    }
+
+    // Setting saltRounds to 10 as it is what I have found is recommended but it is also different from app to app
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const result = await pool.query(`
+        INSERT INTO
+        accounts (account_name, email, password, salt)
+        VALUES (?, ?, ?, ?)`, [username, email, hashedPassword, salt]);
+    
+        return result;
+}
+
+async function dropExpiredEvents(eventIds){
+
+    // Maps the number of event ids to corresponding ?s for prepared statement setup
+    const placeholders = eventIds.map(() => "?").join(", ");
+    const query = `DELETE FROM events WHERE eventid IN (${placeholders})`;
+
+    const result = await pool.query(query, eventIds);
+    return result;
+}
+
+async function verifyLogin(username, password){
+
+    const existing_account = await getAccount(username);
+
+    if(!existing_account){
+        return false; // No user found case
+    }
+
+    const salt = await pool.query(`
+        SELECT salt
+        FROM accounts
+        WHERE username = ?`, [username]);
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const isMatch = await bcrypt.compare(password, hashedPassword);
+    
+    // These returns are subject to change depending on what would be most usefully returned
+
+    if (isMatch) {
+      return true; // Legal username and password 
+    } else {
+      return false; // Invalid username and password
+    }
+}
 
 insertEventsFromScrape();
+
+// Testing on Server
+
+const setEvents = new Set([25625, 30582, 27740]);
+dropExpiredEvents(setEvents);
+
+console.log(getEvents());
+
+createAccount("test1", "email@email.com", "password");
+
+console.log("Legal Login");
+console.log(verifyLogin("test1", "password"));
+console.log("Illegal Login (password)");
+console.log(verifyLogin("test1", "passwod"));
+console.log("Illegal Login (username)");
+console.log(verifyLogin("test", "password"));
