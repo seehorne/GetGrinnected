e8daa7e786a4ccca2f0dded0f2dad2a6f0a0e390
Ellie Seehorn <seehorne@gmail.com>
2025-04-03 23:18:19 -0500

    Change filepath typo

--- src/backend/drop_expired.js
+++ src/backend/drop_expired.js
@@ -1,5 +1,5 @@
 const scrape = require('./scrape.js');
-const db = require('./db.js');
+const db = require('./db_connect.js');
 const fs = require("fs");
 
 db.dropExpiredEvents(scrape.dropPastEvents(scrape.TRUEPATH));
