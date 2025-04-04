066bfac2a23fca7e1e65149720b2149327c0a0d0
Ellie Seehorn <seehorne@gmail.com>
2025-04-04 10:44:23 -0500

    Wait on async, don't wait not async
    had reversed on initial commit, fixing

--- src/backend/drop_expired.js
+++ src/backend/drop_expired.js
@@ -2,5 +2,5 @@ const scrape = require('./scrape.js');
 const db = require('./db_connect.js');
 const fs = require("fs");
 
-let dropTags = await scrape.dropPastEvents(scrape.TRUEPATH);
-db.dropExpiredEvents(dropTags);
+let dropTags = scrape.dropPastEvents(scrape.TRUEPATH);
+await db.dropExpiredEvents(dropTags);
