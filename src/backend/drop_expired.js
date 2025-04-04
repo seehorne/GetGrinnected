bb3a0423fd19055b176f464a74e4976fd3dc9a0d
Ellie Seehorn <seehorne@gmail.com>
2025-04-04 10:38:57 -0500

    Make drop handle async

--- src/backend/drop_expired.js
+++ src/backend/drop_expired.js
@@ -2,4 +2,5 @@ const scrape = require('./scrape.js');
 const db = require('./db_connect.js');
 const fs = require("fs");
 
-db.dropExpiredEvents(scrape.dropPastEvents(scrape.TRUEPATH));
+let dropTags = await scrape.dropPastEvents(scrape.TRUEPATH);
+db.dropExpiredEvents(dropTags);
