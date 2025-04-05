67829d217947aae6f15d07a8085efd73370f0b5c
almond Heil <almond@almendra.dev>
2025-04-05 15:29:15 -0500

    Remove awaits from starts of node scripts
    For some reason, this makes Node think they are ES files and gives
    errors pertaining to that rather than the fact that you can't `await` if
    you're not in a function.
    
    The solution is just to not await outside a function, turns out.

--- src/backend/drop_expired.js
+++ src/backend/drop_expired.js
@@ -2,4 +2,4 @@ const scrape = require('./scrape.js');
 const db = require('./db_connect.js');
 
 let dropTags = scrape.dropPastEvents(scrape.TRUEPATH);
-await db.dropExpiredEvents(dropTags);
+db.dropExpiredEvents(dropTags);
