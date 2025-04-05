66b7eff7e1b03057e73d28139b5e031142e07f06
almond Heil <almond@almendra.dev>
2025-04-05 15:32:07 -0500

    Make daily events script print error to screen again

--- update_daily_events.sh
+++ update_daily_events.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/bash
 cd ~/ROOT
-echo "START $(date --iso-8601=seconds)" &> ~/log/update_daily_events.log
-npm ci &>> ~/log/update_daily_events.log
-npm run drop &>> ~/log/update_daily_events.log
-npm run get_events &>> ~/log/update_daily_events.log
-npm run add_to_db &>> ~/log/update_daily_events.log
-echo "END $(date --iso-8601=seconds)" &>> ~/log/update_daily_events.log
+
+# log all stdout and stderr into ~/log/update_daily_events.log, as well as printing to screen
+echo "START $(date --iso-8601=seconds)" |& tee ~/log/update_daily_events.log
+npm ci                                  |& tee -a ~/log/update_daily_events.log
+npm run drop                            |& tee -a ~/log/update_daily_events.log
+npm run get_events                      |& tee -a ~/log/update_daily_events.log
+npm run add_to_db                       |& tee -a ~/log/update_daily_events.log
+echo "END $(date --iso-8601=seconds)"   |& tee -a ~/log/update_daily_events.log
