5eb2c729f929fd38d98e8b39b2d2d5321991fc8e
Ellie Seehorn <seehorne@gmail.com>
2025-04-07 11:47:17 -0500

    timeout in Bash
    what if? bc they run fine they just don't end

--- update_daily_events.sh
+++ update_daily_events.sh
@@ -5,9 +5,9 @@ npm ci &>> ~/log/update_daily_events.log
 # drop expired events from json, make json of dropped IDs
 npm run drop_file &>> ~/log/update_daily_events.log
 # drop expired events from database
-npm run drop_db &>> ~/log/update_daily_events.log
+timeout 10 npm run drop_db &>> ~/log/update_daily_events.log
 #add new events to JSON
 npm run get_events &>> ~/log/update_daily_events.log
 #use JSON to add new events to database
-npm run add_to_db &>> ~/log/update_daily_events.log
+timeout 10 npm run add_to_db &>> ~/log/update_daily_events.log
 echo "END $(date --iso-8601=seconds)" &>> ~/log/update_daily_events.log
