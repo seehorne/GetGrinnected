148e257a3501fd0396cb6ab9e83d639790012022
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2025-04-09 11:32:01 -0500

    Fixing configuration of XCODE. Followed tutorial. Turns out, the ViewInspector was incorrectly linked to the main app! Unlink it in the project settings (blue icon)

--- src/ios/GetGrinnected/GetGrinnected/Components/EventCards.swift
+++ src/ios/GetGrinnected/GetGrinnected/Components/EventCards.swift
@@ -131,7 +131,7 @@ struct EventCards: View {
 struct EventCards_Previews: PreviewProvider {
     static var previews: some View {
         let  myjson = "[{\"eventid\":28273,\"event_name\":\"SGA Concert\",\"event_description\":\"No description available\",\"event_location\":\"Main Hall Gardner Lounge\",\"organizations\":[\"Sga Concerts\"],\"rsvp\":0,\"event_date\":\"April 9\",\"event_time\":\"7 p.m. - 10 p.m.\",\"event_all_day\":0,\"event_start_time\":\"2025-04-10T00:00:00.000Z\",\"event_end_time\":\"2025-04-10T03:00:00.000Z\",\"tags\":[\"Music\",\"Student Activity\",\"Alumni\",\"Faculty &amp; Staff\",\"General Public\",\"Prospective Students\",\"Student Families\",\"Students\"],\"event_private\":0,\"repeats\":0,\"event_image\":null,\"is_draft\":0},{\"eventid\":30810,\"event_name\":\"Concerts\",\"event_description\":\"\\n  Tabling for Starcleaner Reunion\\n\",\"event_location\":\"Rosenfield Center 1st Floor Lobby - Table 4\",\"organizations\":[\"Sga Concerts\"],\"rsvp\":0,\"event_date\":\"April 8\",\"event_time\":\"11 a.m. - 1 p.m.\",\"event_all_day\":0,\"event_start_time\":\"2025-04-08T16:00:00.000Z\",\"event_end_time\":\"2025-04-08T18:00:00.000Z\",\"tags\":[\"Music\",\"Student Activity\",\"Students\"],\"event_private\":0,\"repeats\":0,\"event_image\":null,\"is_draft\":0}]"
-            var myEvents = EventData.parseEvents(json: myjson)
+            let myEvents = EventData.parseEvents(json: myjson)
         EventCards(myData: myEvents[0])
     }
 }
