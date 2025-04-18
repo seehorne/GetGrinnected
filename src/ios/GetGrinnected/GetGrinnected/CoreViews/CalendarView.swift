fa6739232b2e56efa832a7f1c44084bd7680a989
TimeBudhil <71571047+TimeBudhil@users.noreply.github.com>
2025-04-18 11:10:57 -0500

    added comments to calendar view. Some components taken from this tutorial https://www.youtube.com/watch?v=RvTPVFFzYvg&ab_channel=DesignCode Adjusted to our needs! (we don't need a day timeline that contains the time as we go on, (watch video for details of that)

--- src/ios/GetGrinnected/GetGrinnected/CoreViews/CalendarView.swift
+++ src/ios/GetGrinnected/GetGrinnected/CoreViews/CalendarView.swift
@@ -8,132 +8,45 @@
 import Foundation
 import SwiftUI
 
+//Calendar view
 struct CalendarView: View {
-    @State private var isShowingEventModal = false
+    //Show selected date
     @State private var selectedDate = Date()
     
+    //Main view body
     var body: some View {
+        //geomtry view to have header
         GeometryReader{proxy in
+            //no safe area so the header goes all the way to the top
             let safeAreaTop = proxy.safeAreaInsets.top
+            //vertical scroll view to see mroe events
             ScrollView(.vertical, showsIndicators: false){
+                //vstack of header and events
                 VStack(){
+                    //have search bar on here.
                     Header(safeAreaTop, title: "Calendar", searchBarOn: true)
                     
+                    //vstack to have some spacing between header and main compoments
                     VStack(spacing: 16) {
                         
                         WeekView(selectedDate: $selectedDate)
                             .padding(.bottom, 4)
                         
+                        //event list view for all the events (may have to pass in some arguments according to the day
                         EventListView()
                     }
-                    .padding(.top)
+                    .padding(.top)//padding on top
                     
                 }
                 
-            }
+            }//scroll view
             .edgesIgnoringSafeArea(.top)
             
-            
-            
-            
         }//geometry reader
-    }
+    }//main body view
 }
 
 
-
-
-struct TimelineView: View {
-    let hourSpacing: CGFloat
-    //maybe don't need!
-    var body: some View {
-        VStack(alignment: .trailing, spacing: hourSpacing) {
-            ForEach(0..<25) { hour in
-                TimelineHourView(hour: hour)
-            }
-        }
-        .padding(.bottom, 40)
-    }
-}//timeline view
-
-struct TimelineHourView: View {
-    let hour: Int
-    
-    private var timeString: String {
-        String(format: "%d:00", hour == 0 ? 12 : (hour > 12 ? hour - 12 : hour))
-    }
-    
-    private var amPm: String {
-        hour < 12 ? "AM" : "PM"
-    }
-    
-    var body: some View {
-        VStack(alignment: .trailing) {
-            Text(timeString)
-            Text(amPm)
-        }
-        .font(.footnote)
-        .fontWeight(.semibold)
-        .foregroundColor(.appTextPrimary)
-        .frame(height: 30)
-        .id("hour-\(hour)")
-    }
-}//timeline hour view
-
-
-struct CurrentTimeIndicator: View {
-    @State private var currentTime = Date()
-    let timer = Timer.publish(every: 60, on: .main, in: .common).autoconnect()
-    
-    private var timeString: String {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "h:mm"
-        return formatter.string(from: currentTime)
-    }
-    
-    let hourSpacing: CGFloat
-    private let timelineHourHeight: CGFloat = 30
-    
-    private var indicatorOffset: CGFloat {
-        let calendar = Calendar.current
-        let components = calendar.dateComponents([.hour, .minute], from: currentTime)
-        let hour = CGFloat(components.hour ?? 0)
-        let minute = CGFloat(components.minute ?? 0)
-        
-        return (hour + minute / 60) * (hourSpacing + timelineHourHeight)
-    }
-    
-    var body: some View {
-        HStack {
-            ZStack(alignment: .top) {
-                Rectangle()
-                    .fill(Color.appBackground)
-                    .frame(width: 4)
-                    .cornerRadius(2)
-                
-                Rectangle()
-                    .fill(Color.colorRed)
-                    .frame(width: 4, height: indicatorOffset + 10)
-                    .cornerRadius(2)
-                
-                ZStack {
-                    RoundedRectangle(cornerRadius: 4)
-                        .fill(.colorRed)
-                        .frame(width: 35, height: 20)
-                    
-                    Text(timeString)
-                        .font(.caption2)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.appTextPrimary)
-                }
-                .offset(y: indicatorOffset)
-            }
-        }
-        .onReceive(timer) { _ in
-            currentTime = Date()
-        }
-    }
-}//determine current time
 
 
 
@@ -146,9 +59,4 @@ struct CurrentTimeIndicator: View {
 #Preview("Dark Mode") {
     CalendarView()
         .preferredColorScheme(.dark)
-}
-
-
-#Preview {
-    CalendarView()
 }
