9ebfbd91048d4de07227a05782b85ad8a842cd6b
Ellie Seehorn <seehorne@gmail.com>
2025-05-01 16:33:24 -0500

    Update to refactored routes

--- src/ios/GetGrinnected/GetGrinnected/Database/UserProfile.swift
+++ src/ios/GetGrinnected/GetGrinnected/Database/UserProfile.swift
@@ -80,7 +80,7 @@ class UserProfile: ObservableObject {
     }
 
     func loginUser(email: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/user/login")!
+        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/session/login")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -124,7 +124,7 @@ class UserProfile: ObservableObject {
     }
     
     func verifyUser(email: String, code: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/user/verify")!
+        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/session/verify")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -180,7 +180,7 @@ class UserProfile: ObservableObject {
     
     
     func signUpUser(email: String, user: String, completion: @escaping (Result<String, Error>) -> Void) {
-        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/user/signup")!
+        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/session/signup")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -227,7 +227,7 @@ class UserProfile: ObservableObject {
     //API call to resend OTP
     func resendOTP(email: String, completion: @escaping (Result<String, Error>) -> Void) {
         //make post request to correct route
-        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/user/resend-otp")!
+        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/session/resend-code")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
@@ -294,7 +294,7 @@ class UserProfile: ObservableObject {
             completion(.failure(APIError.unauthorized))
             return
         }
-        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/user/token-refresh")!
+        let url = URL(string: "https://node16049-csc324--spring2025.us.reclaim.cloud/session/refresh")!
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.setValue("Bearer \(refreshToken)", forHTTPHeaderField: "Authorization")
