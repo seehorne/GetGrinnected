a7a3889aa899c2caef7a1256df93e34d3a73f1ce
almond Heil <almond@almendra.dev>
2025-05-13 14:32:41 -0500

    Initial implementation, prints out expired codes

---
+++ src/backend/clean_otp_codes.cjs
@@ -0,0 +1,31 @@
+const sqlite3 = require('sqlite3');
+const DBPATH = './src/backend/Database/localOTP.db'
+
+async function otpFileClean(filename) {
+    // Format the current time in the same format as the database
+    //   example: 2025-05-13T18:51:51.899Z
+    const now = new Date().toISOString()
+
+    // Create a database connection
+    const db = new sqlite3.Database(filename);
+
+    // Delete all expired events.
+    // We can do this in alphabet order, since ISO A < ISO B means that ISO A happened first
+    const sql = `SELECT * FROM data WHERE expire < ?`
+    db.get(sql, now,
+        // callback function for the db command
+        (err, rows) => {
+            if (err) {
+                console.error(err);
+            } else {
+                console.log(`got rows ${JSON.stringify(rows)}`);
+            }
+        });
+}
+
+// when this file is run act like a script, but export our function otherwise
+if (require.main === module) {
+    otpFileClean(DBPATH);
+} else {
+    module.exports = { otpFileClean };
+}
